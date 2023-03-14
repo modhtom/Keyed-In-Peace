2c4c25424837b8973b347e111ef06830c7f788ca
modhtom <85494929+modhtom@users.noreply.github.com>
2023-03-14 15:44:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function playSound(id,value) {
+  var sound = document.getElementById(id);
+  if(value!=0)
+  sound.play();
+  else
+  sound.stop();
+}
+
+function setVolume(id, volume) {
+  var sound = document.getElementById(id);
+  sound.volume = volume;
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    document.querySelector(".float-button").style.display = "block";
+  } else {
+    document.querySelector(".float-button").style.display = "none";
+  }
+}
---
+++ script_2.js
@@ -0,0 +1,27 @@
+// Add a click event listener to each section title
+const sectionTitles = document.querySelectorAll("section h2");
+sectionTitles.forEach(title => {
+	title.addEventListener("click", () => {
+		// Toggle the class "expanded" on the parent section element
+		title.parentNode.classList.toggle("expanded");
+	});
+});
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    document.querySelector(".float-button").style.display = "block";
+  } else {
+    document.querySelector(".float-button").style.display = "none";
+  }
+}
+
+const addButton = document.querySelector(".float-button a");
+
+addButton.addEventListener("click", function() {
+  const newButton = document.createElement("button");
+  newButton.classList.add("new-button");
+  newButton.innerText = "❤️";
+  newButtonsDiv.appendChild(newButton);
+});
